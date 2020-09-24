d14ad6600daa70c6430adc1d9c17e1966e6a5998
Mark <mark.speer11@gmail.com>
2020-09-24 15:48:32 -0400

    commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Eat-Da-Burger
+
+
+Eat-Da-Burger is an app to log burgers you want to eat, and burgers you've already eaten.
+
+utilizing express, handlebars, and mysql
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE o061mve3a0qccy37;
+
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Plain Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Cheese Burger', FALSE);
